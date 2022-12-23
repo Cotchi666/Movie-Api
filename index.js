2b469059e052f6b38f2ff6afeb243ef78086ebd3
CotChi <chiennguyen.170921@gmail.com>
2022-12-23 18:50:52 +0700

    update

---
+++ index.js
@@ -0,0 +1,34 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import http, { Server } from "http";
+import mongoose from "mongoose";
+import "dotenv/config";
+import routes from "./src/routes/index.js";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(cookieParser());
+
+app.use("/api/v1", routes);
+
+const PORT = process.env.PORT || 5000;
+const server = http.createServer(app);
+
+//mongoose.set("strictQuery", true);
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("MONGO CONNECTED");
+    server.listen(PORT, () => {
+      console.log(`Server listening on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+   console.log({ err });
+    process.exit(1);
+  });
